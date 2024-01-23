067faa8fd23cf5e4373993aa41cee36c860673a8
Izaan Khan <izaankhan.x@gmail.com>
2024-01-24 01:17:50 +0500

    letter sorted into array

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+let word = ''
+
 function insertPress(){
-    let letterHolder = []
+    getWord()
 }
 
 function getWord(){
     let word = document.querySelector('.inp-1').value
     console.log(word)
     document.querySelector('.inp-1').value = ''
+    let letterHolder = [];
+    for (let i = 0; i < word.length; i++) {
+        letterHolder.push(word[i]);
+        console.log(word[i]);
+        console.log(letterHolder)
+    }
 }
