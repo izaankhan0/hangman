ee1d333f6e1866e253fa12aa9831f733cacf62a6
Izaan Khan <izaankhan.x@gmail.com>
2024-01-24 01:06:27 +0500

    styling initial css and js

---
+++ script.js
@@ -0,0 +1,9 @@
+function insertPress(){
+    let letterHolder = []
+}
+
+function getWord(){
+    let word = document.querySelector('.inp-1').value
+    console.log(word)
+    document.querySelector('.inp-1').value = ''
+}
