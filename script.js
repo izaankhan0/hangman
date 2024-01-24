096cd515a3b00ab694719c3ee1148ccbaa2feb5d
Izaan Khan <izaankhan.x@gmail.com>
2024-01-24 18:02:08 +0500

    main design and screen design

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let looseCounter = 0;
 
 function insertPress() {
     getWord();
+    document.querySelector('.letters').classList.add('stage-2')
+    document.querySelector('.ans').classList.remove('stage-1')
+
 }
 
 function insertLetter() {
@@ -64,6 +67,7 @@ function addDashes() {
 }
 
 function enterLetter() {
+    
     document.querySelector('.result').innerHTML = "";
     let flag1 = false;
     let char = document.querySelector('.inp-2').value;
@@ -84,7 +88,7 @@ function enterLetter() {
                 let fullDec = document.querySelectorAll('.letterSpace')
                 if (fullDec.length === letterHolder.length){
                     document.querySelector('.screen').classList.add('win')
-                    document.querySelector('.win').innerHTML += `<p>The Word was: " ${word} "</p><h3>You have Won! Congrats!</h3>`
+                    document.querySelector('.win').innerHTML += `<p>The Word was: " ${word.toUpperCase()} "</p><h3>You have Won :D <br> Congrats!</h3>`
                 }
                 
                 
@@ -93,24 +97,35 @@ function enterLetter() {
     } else {
         console.log(char.length);
         alert('Only Enter One letter smartash');
-        document.querySelector('.result').innerHTML = "";
+        // document.querySelector('.result').innerHTML = "";
         document.querySelector('.inp-2').value = '';
+        document.querySelector('.result').innerHTML = `<h2>Enter a Letter</h2><p class="result-p result-p-green">Wrong Letters:  ${looseCounter}/6</p>`;
         return;
     }
 
     if (flag1) {
-        document.querySelector('.result').innerHTML = `<h2 class='present'>Present</h2> <p>Wrong Letters: ${looseCounter}</p>`;
+        document.querySelector('.result').innerHTML = `<h2 class='present'>The Letter "${char.toUpperCase()}" is Present :)</h2> <p class="result-p result-p-green">Wrong Letters:  ${looseCounter}/6</p>`;
     } else {
         looseCounter++
         document.querySelector('.diagram').innerHTML = `<img src="./images/${looseCounter}.png" alt="hangman image">`
-        document.querySelector('.result').innerHTML = `<h2 class='absent'>Absent</h2><p>Wrong Letters: ${looseCounter}</p>`;
+        document.querySelector('.result').innerHTML = `<h2 class='absent'>The Letter "${char.toUpperCase()}" is Absent :(</h2><p class="result-p result-p-green">Wrong Letters:  ${looseCounter}/6</p>`;
         console.log("loose counter is:")
         console.log(looseCounter)
         if (looseCounter === 6){
             document.querySelector('.screen').classList.add('loss')
-                    document.querySelector('.loss').innerHTML += `<p>The Word was: " ${word} "</p><h3>You have Lost! thats sad</h3>`
+                    document.querySelector('.loss').innerHTML += `<p>The Word was: " ${word.toUpperCase()} "</p><h3>You have Lost! thats sad</h3>`
         }
 
     }
     document.querySelector('.inp-2').value = ''
+
+    const resultP = document.querySelector('.result-p');
+    
+        if (looseCounter === 3 || looseCounter === 4) {
+            resultP.className = 'result-p result-p-org';
+        } else if (looseCounter > 4) {
+            resultP.className = 'result-p result-p-red';
+        } else {
+            resultP.className = 'result-p result-p-green';
+        }
 }
